891420835bb6f489f289bd766ae92a56d0450b28
RagedRebel <reubenav1305@gmail.com>
2024-04-28 00:37:24 +0530

    header in progress

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,11 @@
+import logo from "./assets/happytails.png"
+
 function Header(){
 
     return(
-        <div className="bg-rose-600 h-20">
-            <h1 className="text-white text-3xl font-bold font-logo" >HappyTails</h1>
-
+        <div className="bg-rose-600 h-20 flex justify-start items-center mb-5">
+            <img src={logo} alt="logo img" className="size-20 " ></img>
+            <p className="text-white text-5xl font-bold font-logo text-center" >HappyTails</p>
         </div>
              
         
