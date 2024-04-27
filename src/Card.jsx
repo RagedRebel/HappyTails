4cb9b72ab1c31de4431ccf72d68cbfd44c3d0713
RagedRebel <reubenav1305@gmail.com>
2024-04-27 22:47:46 +0530

    fixed css

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,8 +4,8 @@ import dog from "./assets/germanshepherd.jpeg";
 function Card() {
   return (
     <>
-      <h2>Dog</h2>
-      <p className="text-gray-600">German Shepherd</p>
+      <h2 className="text-xl font-bold bg-red-700">Dog</h2>
+      <p className="text-gray-600 bg-red-800">German Shepherd</p>
       <img className="h-10 w-10" src={dog} alt="Image" />
     </>
   );
