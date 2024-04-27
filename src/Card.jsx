8eb339bcd483258cb199fb742574cccee4b211a0
RagedRebel <reubenav1305@gmail.com>
2024-04-27 23:39:00 +0530

    centredddddd

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,11 +3,11 @@ import dog from "./assets/germanshepherd.jpeg";
 
 function Card() {
   return (
-    <>
-      <h2 className="text-xl font-bold bg-red-700">Dog</h2>
-      <p className="text-gray-600 bg-red-800">German Shepherd</p>
-      <img className="h-10 w-10" src={dog} alt="Image" />
-    </>
+    <div className="border-black flex flex-col justify-center items-center w-32  border rounded p-2 m-2 shadow-lg">
+      <img className="h-20 w-20 rounded-full " src={dog} alt="Image" />
+      <h2 className="text-xl">Dog</h2>
+      <p className="text-gray-600 text-sm">German Shepherd</p>
+    </div>
   );
 }
 
