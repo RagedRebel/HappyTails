d6f7ba249858d0dab3919494a14021cc46dffb28
RagedRebel <reubenav1305@gmail.com>
2024-04-28 18:04:55 +0530

    footer almost

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App(){
       <Card/>
       <Card/>
       <Card/>
+      <Footer/>
     
     
   </>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,20 @@
 function Footer(){
     return(
-        <footer>
-            <p>&copy;{new Date().getFullYear()} HappyTales</p>
-        </footer>
+        <>
+            <footer className="absolute inset-x-0 bottom-0 h-16  flex justify-between items-center bg-slate-200 border-t border-slate-400">
+                <p className="text-stone-700 pl-2 " >&copy;{new Date().getFullYear()} HappyTails</p>
+                <ul className="pr-2 flex text-stone-700">
+                    <li className="mr-4 "><a href="#About" className="underline decoration-stone-600">About</a></li>
+                    <li className="mr-4"><a href="#PrivacyPolicy" className="underline  decoration-stone-600">Privacy</a></li>
+                    <li><a href="#Support" className="underline  decoration-stone-600">Support</a></li>
+                </ul>
+                
+
+            </footer>
+            
+
+        </>
+
     );
 }
 export default Footer
