148b778e1d76afeb645c56d56754f2ed1c448324
RagedRebel <reubenav1305@gmail.com>
2024-04-28 18:12:00 +0530

    footer complete

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,9 +4,9 @@ function Footer(){
             <footer className="absolute inset-x-0 bottom-0 h-16  flex justify-between items-center bg-slate-200 border-t border-slate-400">
                 <p className="text-stone-700 pl-2 " >&copy;{new Date().getFullYear()} HappyTails</p>
                 <ul className="pr-2 flex text-stone-700">
-                    <li className="mr-4 "><a href="#About" className="underline decoration-stone-600">About</a></li>
-                    <li className="mr-4"><a href="#PrivacyPolicy" className="underline  decoration-stone-600">Privacy</a></li>
-                    <li><a href="#Support" className="underline  decoration-stone-600">Support</a></li>
+                    <li className="mr-4 hover:text-sky-500"><a href="#About" className="underline decoration-stone-600 hover:decoration-sky-500">About</a></li>
+                    <li className="mr-4 hover:text-sky-500"><a href="#PrivacyPolicy" className="underline  decoration-stone-600 hover:decoration-sky-500">Privacy</a></li>
+                    <li className="hover:text-sky-500"><a href="#Support" className="underline  decoration-stone-600 hover:decoration-sky-500">Support</a></li>
                 </ul>
                 
 
