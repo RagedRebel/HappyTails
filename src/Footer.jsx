265e304ccab9007a9fbe99cbae3d59e36585d016
RagedRebel <reubenav1305@gmail.com>
2024-04-28 18:28:00 +0530

    footer modified

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <>
-            <footer className="absolute inset-x-0 bottom-0 h-16  flex justify-between items-center bg-slate-200 border-t border-slate-400">
+            <footer className="absolute inset-x-0 bottom-0 h-12  flex justify-between items-center bg-slate-200 border-t border-slate-400">
                 <p className="text-stone-700 pl-2 " >&copy;{new Date().getFullYear()} HappyTails</p>
                 <ul className="pr-2 flex text-stone-700">
                     <li className="mr-4 hover:text-sky-500"><a href="#About" className="underline decoration-stone-600 hover:decoration-sky-500">About</a></li>
