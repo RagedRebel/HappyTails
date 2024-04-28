2730d48c770b72976fb9dc66ffd1204b7db3bd5d
RagedRebel <reubenav1305@gmail.com>
2024-04-28 23:01:11 +0530

    props and search

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
 import './app.css'
+import dog from "./assets/germanshepherd.jpeg";
+import cat from "./assets/cat.jpeg";
+
 
 function App(){
   return(
     <>
       <Header/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card img={dog} pet="Dog" breed="German Shepherd"/>
+      <Card img={cat} pet="Cat" breed="Tuxedo"/>
       <Footer/>
     
     
