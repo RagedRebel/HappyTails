57499570552558f33e4265d38a97f8080d502a26
RagedRebel <reubenav1305@gmail.com>
2024-05-01 00:41:37 +0530

    login page progressss

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,13 +3,13 @@
 
 function Login(){
     return (
-<div className="flex justify-center items-center m-auto mb-12 h-screen">
-        <div className="bg-slate-200 shadow-md rounded-lg border-2 border-gray-400 flex-col justify-center items-center flex w-1/5 h-64 ">
-        <label  className="inline-block text-sm font-medium leading-6 text-gray-900 place-self-start ml-7">
+<div className="flex justify-center items-center m-auto h-screen">
+        <div className="bg-slate-200 shadow-md rounded-lg border-2 border-gray-400 flex-col justify-center items-center flex w-72 h-64 ">
+        <label  className="inline-block text-sm font-medium leading-6 text-gray-900 place-self-start ml-5 ">
         Username:
       </label>
         <input type="text" name="Username" className="block w-64 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2 mb-4"/>
-        <label  className="inline-block text-sm font-medium leading-6 text-gray-900 place-self-start ml-7">
+        <label  className="inline-block text-sm font-medium leading-6 text-gray-900 place-self-start ml-5">
         Password:
       </label>
         <input type="password" name="Password" className="block w-64 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-2"/>
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,16 +1,14 @@
 import Footer from "../components/Footer";
 import Login from "../components/Login";
+import Image from "../assets/welcome.jpeg"
 import '../app.css'
 
 
 function Welcome(){
     return(
-        <>
-        
+        <div className="bg-rose-600 overscroll-none">
         <Login/>
         <Footer/>
-        
-        </>
-    );
+        </div>);
 }
-export default Welcome
+export default Welcome;
