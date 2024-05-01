7848b6cd23568ad7d9ff6d121b5f295bd791db34
RagedRebel <reubenav1305@gmail.com>
2024-05-01 12:57:28 +0530

    minor change to searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,7 +2,7 @@ import '../app.css'
 
 function Searchbar(){
     return(
-        <div className=" bg-cyan-200 rounded-full w-5/6 h-10 container mx-auto p-3 flex justify-start items-center">
+        <div className=" bg-cyan-200 rounded-full w-5/6 h-10 container mx-auto p-3 flex justify-start items-center shadow-md">
             <p>Modify Search</p>
 
         </div>
