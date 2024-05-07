0ecb1199e2d571caaf334db2b6e7f17d1f04cc5f
RagedRebel <reubenav1305@gmail.com>
2024-05-07 20:30:30 +0530

    center progressss

--- src/components/pet_desc.jsx
+++ src/components/pet_desc.jsx
@@ -1,18 +1,20 @@
 
 function Petdesc({pet}){
     return(
-      <>
-    <div className="flex justify-center items-center pb-12 h-screen bg-rose-600">
+    
+    <div className="flex-col justify-start items-center pb-12 w-screen h-screen inline-flex bg-rose-600">
+    <div className="place-self-start ">
     <a href="/home" target="_self">
-        <button className="  text-black text p-2 hover:text-sky-500 hover:underline place-self-start " >←Back to Home</button>
+        <p className="  text-black text m-4 hover:text-sky-500 hover:underline text-lg" >←Back to Home</p>
     </a> 
-    <div className="border-black flex-col justify-middle items-center inline-flex border rounded p-4  shadow-lg w-4/5 h-4/5 bg-slate-300">
+    </div>
+    <div className="border-black flex-col justify-middle items-center inline-flex border rounded p-4 mt-4  shadow-lg w-4/5 h-4/5 bg-slate-300">
         <img className="aspect-video max-h-64 rounded-sm shadow-md" src={pet.pet_img} alt="Image" />
         <h2 className="text-5xl font-sans font-semibold p-10 ">{pet.pet_name}</h2>
         <p className="text-gray-600 text-sm italic">{pet.pet_breed}</p>
       </div>
-      </div>
-    </>
+    </div>
+    
     );
 
 }
